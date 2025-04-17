a4c14482cb39ecab680729662f9b36ba9ca328ae
NathanDuggan05 <163123488+NathanDuggan05@users.noreply.github.com>
2025-04-17 23:36:18 +0100

    Service api key and url added

--- src/app/services/last-fm/last-fm.service.ts
+++ src/app/services/last-fm/last-fm.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+
+// import necessary packages
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TicketmasterResult, Event } from './interfaces';
+
+const API_KEY = "50e1f8067cf4a31aa6db277edf2fcb98";
+const API_URL = "https://ws.audioscrobbler.com/2.0/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LastFmService {
+  // inject HttpClient
+  private httpClient = inject(HttpClient);
 
   constructor() { }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { LastFmService } from '../services/last-fm/last-fm.service';
 
 @Component({
   selector: 'app-tab1',
@@ -9,5 +10,7 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
+  private LastFmService = inject(LastFmService);
+
   constructor() {}
 }

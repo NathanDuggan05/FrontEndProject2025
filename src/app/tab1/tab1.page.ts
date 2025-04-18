4e6850bb74c0e383c8cfb46c58b3c79074cc9562
NathanDuggan05 <163123488+NathanDuggan05@users.noreply.github.com>
2025-04-18 01:12:26 +0100

    Tab 1 page showing artists in list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,43 +1,36 @@
-import { Component, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { LastFmService } from '../services/last-fm/last-fm.service';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, NgStyle } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonList, IonItem, IonLabel, FormsModule, NgFor, NgIf, NgStyle, CommonModule]
 })
 export class Tab1Page {
   private lastFmService = inject(LastFmService);
-
-  public songs: any | undefined = undefined; //assigning any/undefined to songs is valid
+  public artists: any[] = []; // Initialize as empty array
 
   constructor() { }
-
 
   ngOnInit() {
     this.lastFmService.getTopArtists().subscribe({
       next: (response: any) => {
-
-        if (response) {
-          this.songs = response;
-
-          // Debug logging for image data structure
-          console.log('Songs:');
-      console.log(this.songs);
+        if (response && response.artists && response.artists.artist) {
+          this.artists = response.artists.artist; // Assign the array of artists
+          console.log('Artists:', this.artists);
         } else {
-          console.error('No song data in the response');
+          console.error('No artist data in the response');
         }
       },
       error: (err) => {
-        console.error('Error fetching event details:', err);
+        console.error('Error fetching artists:', err);
       }
     });
-
-
-
-
   }
 }
